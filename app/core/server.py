74bebff8a5ad6bb02e1fa838714a32df3d172243
cucuridas <cucuridas@gamil.com>
2023-02-07 17:39:34 +0900

    server.py에 webhook 생성 로직 추가

--- app/core/server.py
+++ app/core/server.py
@@ -1,9 +1,11 @@
 from typing import Union
 from fastapi import FastAPI
 from app.api.v1.message import router
+from app.service.webex import WebexHook
 
 
 def createApp() -> FastAPI:
+    WebexHook().addHook()
     fastApiServer = FastAPI()
     fastApiServer.include_router(router)
     return fastApiServer
